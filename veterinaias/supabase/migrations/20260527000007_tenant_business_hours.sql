6a30b5d42f2c12bce688a4232b299778243b3dc9
Carlos Vega <carlos.tests02@gmail.com>
2026-05-27 23:13:49 -0700

    feat: add business_hours to tenants.settings

---
+++ veterinaias/supabase/migrations/20260527000007_tenant_business_hours.sql
@@ -0,0 +1,24 @@
+-- Update tenants.settings default to include business_hours
+ALTER TABLE tenants
+  ALTER COLUMN settings SET DEFAULT '{
+    "confirmation_reminder_days": 2,
+    "share_link_expiry_days": 7,
+    "business_hours": {
+      "days": [1, 2, 3, 4, 5, 6],
+      "start": "09:00",
+      "end": "18:00",
+      "slot_interval": 30
+    }
+  }'::jsonb;
+
+-- Backfill existing rows that don't have business_hours yet
+UPDATE tenants
+SET settings = settings || '{
+  "business_hours": {
+    "days": [1, 2, 3, 4, 5, 6],
+    "start": "09:00",
+    "end": "18:00",
+    "slot_interval": 30
+  }
+}'::jsonb
+WHERE settings -> 'business_hours' IS NULL;
